d7fbc6a902a7123c79e0be0b0a5aa9a1c24e08ec
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-06-14 20:39:20 -0400

    Deleted background.js

--- src/background.js
+++
@@ -1,7 +0,0 @@
-// Default value of volume percentage
-let volumePercentage = 100;
-
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ volumePercentage });
-    console.log('Default volume percentage set to %d%', volumePercentage);
-})
