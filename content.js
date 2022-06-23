f14c6af86182dc5d18a3afdb37dad13a59cbd27b
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-06-23 19:46:58 -0400

    Moved storage to content.js

--- content.js
+++ content.js
@@ -1,28 +1,109 @@
+const VOLUME_KEY = "volume_key";
+const DEFAULT_VOLUME_PERCENTAGE = 100;
+let currentVolumePercentage;
+
 // Gain Node controls the volume of the Media Stream
 let gainNode;
 
-// Add a listener to initialize gainNode and adjust volume
-chrome.runtime.onMessage.addListener(function (request) {
-        if (request.message === "adjust_volume") {
 
-            gainNode = gainNode || firstTimeSetUp();
+initializeCurrentVolumePercentage();
+initializeRequestListeners();
 
-            if (gainNode){
-                console.log("Volume changed: " + request.value);
-                let volumeMultiplier = request.value / 100;
-                if (0 <= volumeMultiplier && volumeMultiplier <= 5) {
-                    gainNode.gain.value = volumeMultiplier;
-                }
+console.log("Now volume: " + currentVolumePercentage);
+
+
+/**
+ * Helper Functions
+ */
+function initializeCurrentVolumePercentage() {
+    currentVolumePercentage = sessionStorage.getItem(VOLUME_KEY);
+
+    console.log("Saved volume: " + currentVolumePercentage);
+
+    // if null, then set as default value and store in localstorage
+    if (currentVolumePercentage == null) {
+        currentVolumePercentage = DEFAULT_VOLUME_PERCENTAGE;
+        sessionStorage.setItem(VOLUME_KEY, String(currentVolumePercentage));
+    }
+    // Do casting here after checking for null.
+    currentVolumePercentage = Number(currentVolumePercentage);
+
+    console.log("Updated volume: " + currentVolumePercentage);
+}
+
+
+/**
+ * Add a listener to initialize gainNode and adjust volume
+ */
+function initializeRequestListeners() {
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+            console.log(request.message);
+            if (request.message === "adjust_volume") {
+                adjustVolume();
+            }
+            if (request.message === "save_volume"){
+                currentVolumePercentage = Number(request.value);
+                sessionStorage.setItem(VOLUME_KEY, currentVolumePercentage);
+            }
+            if (request.message === "get_volume"){
+                sendResponse(currentVolumePercentage);
+            }
+            if (request.message === "reset_volume"){
+                resetVolume();
+                adjustVolume();
+            }
+            if (request.message === "restore_volume"){
+                setTimeout(function(){
+                    gainNode = gainNode || firstTimeSetUp();
+
+                    if (gainNode) {
+                        let volumeMultiplier = Number(sessionStorage.getItem(VOLUME_KEY)) / 100;
+                        if (0 <= volumeMultiplier && volumeMultiplier <= 5) {
+                            gainNode.gain.value = volumeMultiplier;
+                        }
+                    }
+                }, 150)
+
+            }
+            if (request.message === "debug") {
+                console.log(request.value);
             }
         }
-    }
-);
+    );
+}
 
+/**
+ * Adjust volume
+ */
+function adjustVolume(){
+    gainNode = gainNode || firstTimeSetUp();
+
+    if (gainNode) {
+        console.log("Volume changed: " + currentVolumePercentage);
+        let volumeMultiplier = currentVolumePercentage / 100;
+        if (0 <= volumeMultiplier && volumeMultiplier <= 5) {
+            gainNode.gain.value = volumeMultiplier;
+        }
+    }
+}
+
+/**
+ * Resets the VOLUME_KEY of the current tab's sessionStorage to default value.
+ */
+function resetVolume(){
+    sessionStorage.setItem(VOLUME_KEY, String(DEFAULT_VOLUME_PERCENTAGE));
+    currentVolumePercentage = DEFAULT_VOLUME_PERCENTAGE;
+}
+
+/**
+ * Sets up the gain node for the first time.
+ * @returns {null|GainNode}
+ */
 function firstTimeSetUp() {
     // Assume there is only one video element on the HTML page, since querySelector returns the first video.
     const mediaStream = document.querySelector("video");
 
-    if (!mediaStream){
+    if (!mediaStream) {
         return null;
     }
 
@@ -32,6 +113,11 @@ function firstTimeSetUp() {
     return gainNode;
 }
 
+/**
+ * Creates a gain node from the media stream
+ * @param mediaStream - a Media Stream class
+ * @returns {GainNode} - a Gain Node
+ */
 function createGainNodeFromAudioContext(mediaStream) {
     // Create an AudioContext.
     const audioContext = new AudioContext();
